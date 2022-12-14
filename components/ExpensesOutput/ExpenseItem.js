19683ff17c821a19ced11ff43720009c0c249fc2
Zeno Driesen <zeno.driesen@gmail.com>
2022-12-14 14:01:10 +0100

    added some styling

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,15 +1,18 @@
-import { Pressable, Text, View } from "react-native";
+import { StyleSheet, Pressable, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
-function ExpenseItem() {
+function ExpenseItem({ description, amount, date }) {
   return (
     <Pressable>
-      <View>
+      <View style={styles.expenseItem}>
         <View>
-          <Text>DESCRIPTION</Text>
-          <Text>DATE</Text>
+          <Text style={[styles.textBase, styles.description]}>
+            {description}
+          </Text>
+          <Text style={styles.textBase}>{date}</Text>
         </View>
-        <View>
-          <Text>AMOUNT</Text>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amount}>{amount}</Text>
         </View>
       </View>
     </Pressable>
@@ -17,3 +20,39 @@ function ExpenseItem() {
 }
 
 export default ExpenseItem;
+
+const styles = StyleSheet.create({
+  expenseItem: {
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 6,
+    elevation: 3,
+    shadowColor: GlobalStyles.colors.gray500,
+    shadowRadius: 4,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.4,
+  },
+  textBase: {
+    color: GlobalStyles.colors.primary50,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 4,
+    fontWeight: "bold",
+  },
+  amountContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+  },
+  amount: {
+    color: GlobalStyles.colors.primary500,
+    fontWeight: "bold",
+  },
+});
