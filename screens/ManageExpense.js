46aec792b3bb131198530fc7db7b814696171fb0
Zeno Driesen <zeno.driesen@gmail.com>
2022-12-14 13:48:44 +0100

    created screens added All expenses screen added manage expenses screen added recent expenses screen

---
+++ screens/ManageExpense.js
@@ -0,0 +1,8 @@
+import {Text} from "react-native";
+
+function ManageExpense() {
+    return <Text>ManageExpense screen</Text>
+
+}
+
+export default ManageExpense;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,7 @@
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+
+function RecentExpenses() {
+  return <ExpensesOutput expensesPeriod="Last 7 days" />;
+}
+
+export default RecentExpenses;
