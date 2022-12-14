89f2ffe16e22786b3a043d840e8d773b951eeb4a
Zeno Driesen <zeno.driesen@gmail.com>
2022-12-14 16:49:59 +0100

    app wide state using context using context from inside components"

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,80 +4,11 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2022-12-14"),
-  },
-  {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 89.29,
-    date: new Date("2022-10-25"),
-  },
-  {
-    id: "e3",
-    description: "Some bananas",
-    amount: 5.99,
-    date: new Date("2022-11-29"),
-  },
-  {
-    id: "e4",
-    description: "A book",
-    amount: 14.99,
-    date: new Date("2022-09-14"),
-  },
-  {
-    id: "e5",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2022-09-15"),
-  },
-  {
-    id: "e6",
-    description: "Some bananas",
-    amount: 5.99,
-    date: new Date("2022-11-29"),
-  },
-  {
-    id: "e7",
-    description: "A book",
-    amount: 14.99,
-    date: new Date("2022-09-14"),
-  },
-  {
-    id: "e8",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2022-09-15"),
-  },
-  {
-    id: "e9",
-    description: "Some bananas",
-    amount: 5.99,
-    date: new Date("2022-11-29"),
-  },
-  {
-    id: "e10",
-    description: "A book",
-    amount: 14.99,
-    date: new Date("2022-09-14"),
-  },
-  {
-    id: "e11",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2022-09-15"),
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
-      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesList expenses={expenses} />
     </View>
   );
 }
