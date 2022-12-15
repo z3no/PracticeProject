6cfb5f683dba0278e4a4f4ffb2d86c8e7b8e5ae2
Zeno Driesen <zeno.driesen@gmail.com>
2022-12-15 10:53:23 +0100

    added fallback text

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -10,11 +10,15 @@ function RecentExpenses() {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
 
-    return expense.date > date7DaysAgo;
+    return expense.date >= date7DaysAgo && expense.date <= today;
   });
 
   return (
-    <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 days" />
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 days"
+      fallbackText="No expenses registered for the last 7 days."
+    />
   );
 }
 
